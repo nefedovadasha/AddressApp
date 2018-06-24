7736dedbef69b13ae58261124217d6aefd0f4b65
dasha <nefedovadasha@gmail.com>
2018-06-24 18:05:20 +0300

    Add information about person and binding class-controller to the fxml-file

--- src/ru/nefedovadasha/address/MainApp.java
+++ src/ru/nefedovadasha/address/MainApp.java
@@ -1,11 +1,15 @@
 package ru.nefedovadasha.address;
 
 import javafx.application.Application;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
 import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.BorderPane;
 import javafx.stage.Stage;
+import ru.nefedovadasha.address.model.Person;
+import ru.nefedovadasha.address.view.PersonOverviewController;
 
 import java.io.IOException;
 
@@ -13,6 +17,25 @@ public class MainApp extends Application {
 
     private Stage primaryStage;
     private BorderPane rootLayout;
+
+    private ObservableList<Person> personData = FXCollections.observableArrayList();
+
+    public MainApp() {
+        // В качестве образца добавляем некоторые данные
+        personData.add(new Person("Hans", "Muster"));
+        personData.add(new Person("Ruth", "Mueller"));
+        personData.add(new Person("Heinz", "Kurz"));
+        personData.add(new Person("Cornelia", "Meier"));
+        personData.add(new Person("Werner", "Meyer"));
+        personData.add(new Person("Lydia", "Kunz"));
+        personData.add(new Person("Anna", "Best"));
+        personData.add(new Person("Stefan", "Meier"));
+        personData.add(new Person("Martin", "Mueller"));
+    }
+
+    public ObservableList<Person> getPersonData() {
+        return personData;
+    }
 
     @Override
     public void start(Stage primaryStage) {
@@ -46,6 +69,9 @@ public class MainApp extends Application {
             AnchorPane personOverview = (AnchorPane) loader.load();
 
             rootLayout.setCenter(personOverview);
+
+            PersonOverviewController controller = loader.getController();
+            controller.setMainApp(this);
         }
         catch (IOException e){
 
---
+++ src/ru/nefedovadasha/address/view/PersonOverviewController.java
@@ -0,0 +1,47 @@
+package ru.nefedovadasha.address.view;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import ru.nefedovadasha.address.MainApp;
+import ru.nefedovadasha.address.model.Person;
+
+public class PersonOverviewController {
+    @FXML
+    private TableView<Person> personTable;
+    @FXML
+    private TableColumn<Person, String> firstNameColumn;
+    @FXML
+    private TableColumn<Person, String> lastNameColumn;
+
+    @FXML
+    private Label firstNameLabel;
+    @FXML
+    private Label lastNameLabel;
+    @FXML
+    private Label streetLabel;
+    @FXML
+    private Label postalCodeLabel;
+    @FXML
+    private Label cityLabel;
+    @FXML
+    private Label birthdayLabel;
+
+    private MainApp mainApp;
+
+    public PersonOverviewController(){
+    }
+
+    @FXML
+    private void initialize(){
+        firstNameColumn.setCellValueFactory(cellData -> cellData.getValue().firstNameProperty());
+        lastNameColumn.setCellValueFactory(cellData -> cellData.getValue().lastNameProperty());
+    }
+
+    public void setMainApp(MainApp mainApp) {
+        this.mainApp = mainApp;
+
+        personTable.setItems(mainApp.getPersonData());
+    }
+}
